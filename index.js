0ce4ce89f0e5e0184a2958e15ca7c5a521e52c0b
tray <onesimus.githua@student.moringaschool.com>
2023-10-08 11:52:48 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
-/* Your Code Here */
+function createTimeInEvent(dateStamp) {
+    const [date, hour] = dateStamp.split(' ');
+  
+    this.timeInEvents.push({
+      type: 'TimeIn',
+      hour: parseInt(hour, 10),
+      date: date
+    });
+  
+    return this;
+  }
 
-/*
- We're giving you this function. Take a look at it, you might see some usage
- that's new and different. That's because we're avoiding a well-known, but
- sneaky bug that we'll cover in the next few lessons!
-
- As a result, the lessons for this function will pass *and* it will be available
- for you to use if you need it!
- */
 
 const allWagesFor = function () {
     const eligibleDates = this.timeInEvents.map(function (e) {
@@ -21,3 +23,16 @@ const allWagesFor = function () {
     return payable
 }
 
+function wagesEarnedOnDate(date) {
+    const hoursWorked = hoursWorkedOnDate.call(this, date);
+    const payRate = this.payPerHour;
+    return hoursWorked * payRate;
+  }
+  function findEmployeeByFirstName(srcArray, firstName) {
+    return srcArray.find(employee => employee.firstName === firstName);
+  }
+  function calculatePayroll(employeeRecords) {
+    return employeeRecords.reduce((totalPay, employee) => {
+      return totalPay + allWagesFor.call(employee);
+    }, 0);
+  }
